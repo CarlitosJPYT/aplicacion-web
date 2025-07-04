5298dc8ab84ed31c0f1780b555026d758f347fa5
Carlitos <juanpana10ospina@gmail.com~>
2025-07-04 17:48:51 -0500

    push

---
+++ public/target/debug/deps/public.d
@@ -0,0 +1,5 @@
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\aplicacion-web\public\target\debug\deps\public.d: src\main.rs
+
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\aplicacion-web\public\target\debug\deps\public.exe: src\main.rs
+
+src\main.rs:
---
+++ public/target/debug/public.d
@@ -0,0 +1,1 @@
+C:\Users\LENOVO\Documents\MuseScore3\Imágenes\aplicacion-web\public\target\debug\public.exe: C:\Users\LENOVO\Documents\MuseScore3\Imágenes\aplicacion-web\public\src\main.rs
