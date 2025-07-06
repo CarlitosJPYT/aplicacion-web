c8829a209efdabbf68033a66dd401bd908dc2228
Carlitos <juanpana10ospina@gmail.com~>
2025-07-05 19:39:13 -0500

    add main

--- public/src/index.rs
+++ public/src/index.rs
@@ -1,4 +1,7 @@
 fn main(){
-	let tupla = (3.14, 10, 'p');
-	println!("{}", tupla);
+	let tupla = (3.14, 10, 'p', true);
+	println!("{}", tupla[0]);
+	println!("{}", tupla[1]);
+	println!("{}", tupla[2]);
+	println!("{}", tupla[3]);
 }
