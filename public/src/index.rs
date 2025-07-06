024f257102f012384965cf66645e94a152956a09
Carlitos <juanpana10ospina@gmail.com~>
2025-07-05 20:19:25 -0500

    add master

--- public/src/index.rs
+++ public/src/index.rs
@@ -1,13 +1,8 @@
 fn main(){
-	let tupla = (3.14, 10, 'p', true);
-	
-	let number = tupla[0];
-	let number1 = tupla[1];
-	let number2 = tupla[2];
-	let number3 = tupla[3];
+	let tupla: (i32, f64, bool, char) = (10, 3.14, true, 'p');
 
-	println!("{}", number);
-	println!("{}", number1);
-	println!("{}", number2);
-	println!("{}", number3);
+	println!("La primera variable es {}", tupla.0);
+	println!("La segunda variable es {}", tupla.1);
+	println!("La tercera variable es {}", tupla.2);
+	println!("La ultima variable es {}", tupla.3);
 }
