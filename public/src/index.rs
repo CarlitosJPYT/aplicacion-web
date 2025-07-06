cd542c19cfc0b82b0a592f6660e33b01589cb808
Carlitos <juanpana10ospina@gmail.com~>
2025-07-05 20:10:45 -0500

    new master

--- public/src/index.rs
+++ public/src/index.rs
@@ -1,7 +1,13 @@
 fn main(){
 	let tupla = (3.14, 10, 'p', true);
-	println!("{}", tupla[0]);
-	println!("{}", tupla[1]);
-	println!("{}", tupla[2]);
-	println!("{}", tupla[3]);
+	
+	let number = tupla[0];
+	let number1 = tupla[1];
+	let number2 = tupla[2];
+	let number3 = tupla[3];
+
+	println!("{}", number);
+	println!("{}", number1);
+	println!("{}", number2);
+	println!("{}", number3);
 }
